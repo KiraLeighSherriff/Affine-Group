a5649f60ff5382635555044a9da1f1228685e97e
KiraLeighSherriff <124630003+KiraLeighSherriff@users.noreply.github.com>
2023-03-21 23:42:28 +0000

    Update Affine_Cipher.c
    The Final Version of The Code.

--- Affine_Cipher.c
+++ Affine_Cipher.c
@@ -1,3 +1,19 @@
+/****************************************************
+*             De Montfort University                /
+*              Cryptography CTEC2915                /
+*        Affine Cipher for both brute froce &       /
+*              encryption & Decryption              /
+*****************************************************/
+
+/****************************************************
+*                   Created by                      /
+*              p2531028 Aryan Gill                  /
+*              p2519032 Jasmin Hayre                /
+*              p2668258 Faheem Khan                 /
+*              p2660920 Kira Sherriff               /
+*           All team members contributed            /
+*****************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,155 +29,184 @@ char removeNonLetters(char *plaintext);
 
 int main()
 {
-    char plaintext[300]; // stores the user input text 
+
+    char plaintext[300]; 
     int a, b; // keys 
-    int option, menu; 
+    int option, menu;
+
+    option = menu = 0;
 
     /* 
+
     options = used for Case 1 to let the use choose to 
     encrypted or decrypted 
      
     menu = Lets the user choose between case 1 and case 2.
-    */
 
-    option = menu = 0;
-
-    
-    // Get user input to know if they want to brute force or encrypted/decrypted
-    printf("Enter 1 for encrypted or decrypted with keys\n");
-    printf("Enter 2 for Brute Froce of affiner cipher\n");
-    printf("Enter: ");
-    scanf("%d", &menu);
-
-    /* 
     Case 1 is for encrypted or decrypted with keys when the keys are known to the user
     also can be used to encryt a text. User can give thier own keys.
 
     Case 2 is for brute forece, used for when the keys are unknown. 
     Group cipher text =  RILTOEAQGEQDPOJOESPQW
+
     */
 
-    switch(menu)
-    {
+    do {
 
-    /*--------------------------------Encrytion/Decryption-------------------------------------------*/
-    case 1:
+        printf("\nEnter 0 to exit the program\n");
+        printf("Enter 1 for encrypted or decrypted with keys\n");
+        printf("Enter 2 for Brute Froce of affiner cipher\n");
+        printf("Enter: ");
+        scanf("%d", &menu);
 
-        // Getting user input
-        printf("Enter the text to be encrypted or decrypted: ");
-        scanf("%s", &plaintext);
+        // Clears the input buffer so fegts can run
+        while(getchar() != '\n');
 
-        // Changing input to upper case
-        for (int i = 0; i < strlen(plaintext); i++)
+        switch(menu)
         {
-            plaintext[i] = toupper(plaintext[i]);
-        }
 
-        // Remove numbers and symbolsfrom string
-        removeNonLetters(plaintext);
+            case 0:
 
-        // Getting the keys
-        printf("Enter key 1 (1 < 26): ");
-        scanf("%d", &a);
+                exit(0);
 
-        // If the key is not relatively prime to 26 loop until input it is
-        // GCD number are 1,3,5,7,9,11,15,17,19,23,25,
-        while((gcd(a, 26) != 1) || (a >= 26))
-        {
-            printf("Invalid value of a. Enter a one value that is relatively prime to 26: ");
+            break;
+                
+/*------------------------------------Encrytion/Decryption-----------------------------------------------------*/
+            case 1:
+
+            // Getting user input
+            printf("Enter the text to be encrypted or decrypted: ");
+            fgets(plaintext, sizeof(plaintext), stdin);
+
+            // Text to upper case
+            for (int i = 0; i < strlen(plaintext); i++) 
+            {
+                plaintext[i] = toupper(plaintext[i]);
+            }
+
+            removeNonLetters(plaintext);
+
+            // Getting key a 
+            printf("Enter key a: ");
             scanf("%d", &a);
-        }
 
-        printf("Enter key 2 (0 < 25): : ");
-        scanf("%d", &b);
+            // If the key is not relatively prime to 26 loop until input it is
+            // GCD number are 1,3,5,7,9,11,15,17,19,23,25,
+            while (gcd(a, 26) != 1)
+            {
+                printf("Invalid key for a! Key of a needs to relatively prime to 26: ");
+                scanf("%d", &a);
+            }
 
-        // Letting the user Choose if they want to decrypt or encrypt
-        // If 1 Entered Encryption 
-        // If 2 Entered Decrypt
-        while((option != 1) && (option != 2))
-        {
-        
-            printf("Enter 1 to encrypt or 2 to decrypt: ");
-            scanf("%d", &option);
+            printf("Enter key b: ");
+            scanf("%d", &b);
 
-            if (option == 1)
+            option = 0;
+
+            // Letting the user choose if they want to decrypt or encrypt
+            // If 1 Entered Encryption 
+            // If 2 Entered Decrypt
+            while((option != 1) && (option != 2))
+            {
+                // Choosing function
+                printf("Enter 1 to encrypt or 2 to decrypt: ");
+                scanf("%d", &option);
+
+                if (option == 1)
                     affineEncrypt(plaintext, a, b);
                 else if (option == 2)
                     affineDecrypt(plaintext, a, b);
                 else
                     printf("Invalid input\n");
-        
-        }
-
-        printf("Result: %s\n", plaintext);
-        
-            break;
-    
-
-/*------------------------------------Brute Force------------------------------------------------*/
-
-    case 2: 
-        
-        printf("Enter the text to decrypted: ");
-        scanf("%s", &plaintext);
-
-        for (int i = 1; i < strlen(plaintext); i++) 
-        {
-            plaintext[i] = toupper(plaintext[i]);
-
-        }
-
-        // Looping though the keys for brutefroce
-        for(a = 1; a < 26; a++)
-            {
-                // Checking keys so only those that are relatively prime to 26
-                if (gcd(a, 26) == 1)   
-                {
-                    for(b= 0; b < 26; ++b)
-                    {
-                            // Calls function
-                            affineBruteFroce(plaintext, a, b); 
-                    }
-                }
             }
 
-        break;
+            printf("\nKeys: a = %d, b = %d \tResult: %s\n", a, b, plaintext);
 
-    default:
 
-        printf("Enter invaid option");
+            break;
+                
+/*------------------------------------Brute Force------------------------------------------------*/
 
-        break;
-    }
+
+            case 2: 
+                
+                printf("Enter the text to decrypted: ");
+                scanf("%s", &plaintext);
+
+                for (int i = 1; i < strlen(plaintext); i++) 
+                {
+                    plaintext[i] = toupper(plaintext[i]);
+
+                }
+
+                // Looping though the keys for brutefroce
+                for(a = 1; a < 26; a++)
+                    {
+                        // Checking keys so only those that are relatively prime to 26
+                        if (gcd(a, 26) == 1)   
+                        {
+                            for(b= 0; b < 26; ++b)
+                            {
+                                    
+                                    affineBruteFroce(plaintext, a, b); 
+                            }
+                        }
+                    }
+
+                break;
+
+
+            // If wrong input is entered
+            default:
+
+                printf("\nEnter invaid! Enter 1 or 2!\n");
+                
+                break;
+            }
+
+    }while (menu != 1 || menu != 2 );
 }
+
+
+/*------------------------------------GCD------------------------------------------------*/
 
 // Function to calculate the GCD of two numbers
+
 int gcd(int a, int b) {
+    // checking if the second input is equal to zero
     if (a==0)
-     { // checking if the second input is equal to zero
+    { 
         return b;
-     }
-       
-    return gcd(b % a, a); // return a in which the gcd is stored 
+    }
+    
+    // Return the calculation of gcd to the functions
+    return gcd(b % a, a); 
 }
 
+/*------------------------------------modInv------------------------------------------------*/
+
 // Function to calculate the modular multiplicative inverse 
+
 int modInv(int a, int m)
 {
-
-   for (int i = 1; i < m; i++)
-     {
-       if ((a * i) % m == 1)
-         {
+   // Loop though to 26
+    for (int i = 1; i < m; i++)
+    {
+        if ((a * i) % m == 1)
+        {
             return i;
-         }
-      }
+        }
+    }
     
    return -1;
 }
+
     
 
+/*------------------------------------removeNonLetters------------------------------------------------*/
+
 // Function to remove numbers and symbols from string
+
 char removeNonLetters(char *plaintext)
 {
     int j, i;
@@ -178,7 +223,12 @@ char removeNonLetters(char *plaintext)
     
 }
 
+
+
+/*------------------------------------affineBruteFroce------------------------------------------------*/
+
 // Function to implement the Brute Force
+
 void affineBruteFroce(char *plaintext, int a, int b)
 {
     int i, modInvCheck, decryt, lengh;
@@ -186,7 +236,7 @@ void affineBruteFroce(char *plaintext, int a, int b)
 
     char character, text[lengh];
 
-    // Check to to see if CoefficientOne is inverted
+    // Check to to see if a is inverted
     modInvCheck = modInv(a, 26);
         if(modInvCheck == 0)
         {
@@ -207,24 +257,66 @@ void affineBruteFroce(char *plaintext, int a, int b)
         if(decryt < 0)
             decryt += 26;
 
-        // Add A for the ASCCI conversion  
+        // Add A for the ASCII conversion  
         text[i] = 'A' + decryt;
     }
 
     text[lengh] = '\0';
 
-    printf("Keys: a = %d, b = %d \tDecryption: %s\n", a, b, text);
+    // Will print out all 312 possible decryptions 
+    printf("\nPrivare Keys: a = %d, b = %d \tResult: %s\n", a, b, text);
 }
 
+
+
+
+/*------------------------------------affineEncrypt------------------------------------------------*/
+
 // Function to implement the affine cipher for encryption
+
+
 void affineEncrypt(char *plaintext, int a, int b)
 {
+    int i; 
+
+    // Replace each letter with the corresponding letter
+    // encrypted using the affine cipher
+    for (i = 0; i < strlen(plaintext); i++)
+    {
+        // For maintaining the in text space
+        if(plaintext[i] == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            plaintext[i] = (a * (plaintext[i] - 'A') + b) % 26 + 'A';
+        }
+    }
     
 }
 
+/*------------------------------------affineDecrypt------------------------------------------------*/
+
 // Function to implement the affine cipher for decryption
+
+
 void affineDecrypt(char *plaintext, int a, int b)
 {
-
+    int i; 
+    
+    // Replace each letter with the corresponding letter
+    // decrypted using the affine cipher
+    for (i = 0; i < strlen(plaintext); i++)
+    {
+        if(plaintext[i] == ' ')
+        {
+            continue;
+        }
+        else
+        {
+            plaintext[i] = (modInv(a, 26) * ((plaintext[i] - 'A') - b + 26)) % 26 + 'A';
+        }
+    }
     
 }
